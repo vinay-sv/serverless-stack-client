8f4cbffc983d490728e23c6eb1eede334412d037
vinay-sv <vinay10990@gmail.com>
2019-11-01 20:42:32 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "Asia Pacific (Mumbai)",
+    BUCKET: "note-related-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://0p23add5d1.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_LtFQU1uph",
+    APP_CLIENT_ID: "6jdns1c97vg3iha0sggfo7hek4",
+    IDENTITY_POOL_ID: "us-east-2:04df7727-9a58-4e13-b4ab-1ed5359e0df4"
+  }
+};
